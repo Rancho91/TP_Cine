4ab47e22642e9f7ed3cf895da11a9a617bd2ef05
jpostillon <joaquinpostillon@gmail.com>
2023-11-01 20:37:12 -0300

    eliminación de advertencias

--- DataCineDb/Data/DbHelper.cs
+++ DataCineDb/Data/DbHelper.cs
@@ -10,13 +10,13 @@ namespace DataCineDb.Data
     public class DbHelper
     {
         private SqlConnection conexion;
-        private string stringconexion = @"Data Source=DESKTOP-1G25HFQ;Initial Catalog=COMPLEJO_CINE;Integrated Security=True";
-        private static DbHelper instancia;
+        private string stringconexion = @"Data Source=DESKTOP-M7J5IEU\SQLEXPRESS;Initial Catalog=COMPLEJO_CINE;Integrated Security=True";
+        private static DbHelper ?instancia;
 
         private DbHelper()
         {
                 
-            conexion = new SqlConnection("Data Source=DESKTOP-1G25HFQ;Initial Catalog=COMPLEJO_CINE;Integrated Security=True");
+            conexion = new SqlConnection(@"Data Source=DESKTOP-M7J5IEU\SQLEXPRESS;Initial Catalog=COMPLEJO_CINE;Integrated Security=True");
 
            
         }
